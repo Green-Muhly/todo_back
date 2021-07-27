df5c3486141ac4033c31edbc5616d2e3e665de0c
Hyunta <mohai2618@naver.com>
2021-07-27 15:38:36 +0900

    feat: Add repository

--- todo/src/main/java/com/greenmuhly/todo/domain/TodoRepository.java
+++ todo/src/main/java/com/greenmuhly/todo/domain/TodoRepository.java
@@ -1,4 +1,34 @@
 package com.greenmuhly.todo.domain;
 
-public class TodoRepository {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+//@Repository
+//@RequiredArgsConstructor
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+//    private final EntityManager em;
+//
+//    public void save(Todo todo) {
+//        if (todo.getId() == null) {
+//            em.persist(todo);
+//        } else {
+//            em.merge(todo);
+//        }
+//    }
+//
+//    public Todo findOne(Long id) {
+//        return em.find(Todo.class, id);
+//    }
+//
+//    public List<Todo> findAll() {
+//        return em.createQuery("select t from Todo t", Todo.class)
+//                .getResultList();
+//    }
+
+
 }
