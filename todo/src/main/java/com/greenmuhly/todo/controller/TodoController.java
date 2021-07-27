92ced3ad1de37f71c9b45e7453441bc7da36e7c8
Hyunta <mohai2618@naver.com>
2021-07-27 15:42:37 +0900

    feat: Add TodoService

---
+++ todo/src/main/java/com/greenmuhly/todo/controller/TodoController.java
@@ -0,0 +1,4 @@
+package com.greenmuhly.todo.controller;
+
+public class TodoController {
+}
--- todo/src/main/java/com/greenmuhly/todo/service/TodoService.java
+++ todo/src/main/java/com/greenmuhly/todo/service/TodoService.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 public interface TodoService {
 
+    List<Todo> getTodos(Sort sort) throws Exception;
+
     void postTodo(Todo todo) throws Exception;
 
-    void deleteTodo(Long Id) throws Exception;
+    void deleteTodo(Long id) throws Exception;
 
-    Todo findTodoById(Long Id) throws Exception;
+    Todo findTodoById(Long id) throws Exception;
 }
