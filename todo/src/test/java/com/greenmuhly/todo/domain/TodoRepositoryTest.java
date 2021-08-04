39c8cc8291db17fccf49359791a96813e70708a7
Hyunta <mohai2618@naver.com>
2021-08-04 18:09:59 +0900

    test: Delete test

--- todo/src/test/java/com/greenmuhly/todo/domain/TodoRepositoryTest.java
+++ todo/src/test/java/com/greenmuhly/todo/domain/TodoRepositoryTest.java
@@ -30,37 +30,37 @@ public class TodoRepositoryTest {
         return em.persist(todo);
     }
 
-    @Test
-    public void getTest() throws Exception {
-        //given
-        Todo todo = getSaved();
-        System.out.println("==================================");
-        System.out.println("todo.getId() = " + todo.getId());
-        System.out.println("todo.getContent() = " + todo.getContent());
-        System.out.println("todo.getChecked() = " + todo.getChecked());
-        System.out.println("todo.getCreateDate() = " + todo.getCreateDate());
-        System.out.println("==================================");
-        em.persist(todo);
-        Long id = todo.getId();
-
-        //when
-        Todo savedTodo = todoRepository.getById(id);
-
-        //then
-        assertThat(savedTodo.getContent()).isEqualTo(content);
-        assertThat(savedTodo.getContent()).isEqualTo(todo.getContent());
-
-    }
-
-    @Test
-    public void deleteTest(){
-        //given
-        Todo todo = getSaved();
-        Long id = todo.getId();
-        //when
-        todoRepository.deleteById(id);
-
-        //then
-        assertThat(em.find(Todo.class, id)).isNull();
-    }
+//    @Test
+//    public void getTest() throws Exception {
+//        //given
+//        Todo todo = getSaved();
+//        System.out.println("==================================");
+//        System.out.println("todo.getId() = " + todo.getId());
+//        System.out.println("todo.getContent() = " + todo.getContent());
+//        System.out.println("todo.getChecked() = " + todo.getChecked());
+//        System.out.println("todo.getCreateDate() = " + todo.getCreateDate());
+//        System.out.println("==================================");
+//        em.persist(todo);
+//        Long id = todo.getId();
+//
+//        //when
+//        Todo savedTodo = todoRepository.getById(id);
+//
+//        //then
+//        assertThat(savedTodo.getContent()).isEqualTo(content);
+//        assertThat(savedTodo.getContent()).isEqualTo(todo.getContent());
+//
+//    }
+//
+//    @Test
+//    public void deleteTest(){
+//        //given
+//        Todo todo = getSaved();
+//        Long id = todo.getId();
+//        //when
+//        todoRepository.deleteById(id);
+//
+//        //then
+//        assertThat(em.find(Todo.class, id)).isNull();
+//    }
 }
